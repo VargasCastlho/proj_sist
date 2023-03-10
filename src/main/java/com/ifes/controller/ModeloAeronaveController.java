792e88823d965875f80a36592c9c0fa7782c4581
2022222760011 <2022222760011@col-8138-li07.cefetes.br>
2023-03-10 14:24:34 -0300

    feat: feito controllers

---
+++ src/main/java/com/ifes/controller/ModeloAeronaveController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifes.controller;
+
+/**
+ *
+ * @author 2022222760011
+ */
+public class ModeloAeronaveController {
+    
+}
