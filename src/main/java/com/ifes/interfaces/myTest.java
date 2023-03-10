9ce392710333949731af128330ca8a8ab933d157
2022222760011 <2022222760011@col-8138-li07.cefetes.br>
2023-03-10 14:25:17 -0300

    feat: classe teste

---
+++ src/main/java/com/ifes/interfaces/myTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifes.interfaces;
+
+import com.ifes.repository.MyDatabaseOperations;
+
+/**
+ *
+ * @author 2022222760011
+ */
+public class myTest {
+    public static void main(String[] args) {
+        MyDatabaseOperations.runExamples();
+    }
+   
+}
